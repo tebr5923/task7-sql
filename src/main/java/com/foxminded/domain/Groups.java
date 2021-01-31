34cd4620e9b38b4fc0636d147239e10fb1c3836b
Dmitry Tebryaev <tebr5923@mail.ru>
2021-01-31 14:08:06 +0700

    add Groups

---
+++ src/main/java/com/foxminded/domain/Groups.java
@@ -0,0 +1,40 @@
+package com.foxminded.domain;
+
+import java.util.Objects;
+
+public class Groups {
+    private int id;
+    private String name;
+
+    public Groups() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Groups groups = (Groups) o;
+        return id == groups.id && Objects.equals(name, groups.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
