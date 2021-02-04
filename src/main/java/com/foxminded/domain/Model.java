81c5ddbf65e05873702c95653b9e4f2a770b2af6
Dmitry Tebryaev <tebr5923@mail.ru>
2021-02-04 20:53:31 +0700

    add Model

---
+++ src/main/java/com/foxminded/domain/Model.java
@@ -0,0 +1,31 @@
+package com.foxminded.domain;
+
+import java.util.Objects;
+
+public abstract class Model {
+    protected int id;
+
+    protected Model() {
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Model model = (Model) o;
+        return id == model.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
