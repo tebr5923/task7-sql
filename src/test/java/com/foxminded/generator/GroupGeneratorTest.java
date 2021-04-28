593e839e968a099f6d38be64aa884f478f63dba8
Dmitry Tebryaev <tebr5923@mail.ru>
2021-04-28 13:18:57 +0700

    refactored without mockRandom

--- src/test/java/com/foxminded/generator/GroupGeneratorTest.java
+++ src/test/java/com/foxminded/generator/GroupGeneratorTest.java
@@ -1,37 +1,28 @@
 package com.foxminded.generator;
 
 import com.foxminded.domain.Group;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 class GroupGeneratorTest {
-    @Mock
-    private Random mockRandom;
-
-    @BeforeEach
-    void setUp() {
-        when(mockRandom.nextInt(Mockito.anyInt())).thenReturn(1);
-    }
+    private static final long seed = 10;
 
     @Test
     void generate_shouldReturnGroupList() {
-        Group group = new Group();
-        group.setName("BB-11");
-        List<Group> expected = Arrays.asList(group, group, group);
+        Group groupPG30 = new Group();
+        groupPG30.setName("PG-30");
+        Group groupES78 = new Group();
+        groupES78.setName("ES-78");
+        Group groupVC39 = new Group();
+        groupVC39.setName("VC-39");
+        List<Group> expected = Arrays.asList(groupPG30, groupES78, groupVC39);
 
-        List<Group> actual = new GroupGenerator(mockRandom).generate(3);
+        List<Group> actual = new GroupGenerator(new Random(seed)).generate(3);
 
         assertEquals(expected, actual);
     }
